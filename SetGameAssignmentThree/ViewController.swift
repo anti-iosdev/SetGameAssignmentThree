9acaa848d3f2bfa9d3d6eff6f31226c60d6366ec
Anti <anti@antis-imac-pro.local>
2018-10-03 16:53:54 -0700

    Initial Commit

---
+++ SetGameAssignmentThree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SetGameAssignmentThree
+//
+//  Created by Anti on 10/3/18.
+//  Copyright © 2018 Anti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
